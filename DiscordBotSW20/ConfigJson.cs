4adb082e97197acbe4d44c4438886442f5431f55
KarmaKamikaze <nicolaijgn@gmail.com>
2020-10-25 18:33:11 +0100

    Add project files.

---
+++ DiscordBotSW20/ConfigJson.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBotSW20
+{
+    public struct ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+        [JsonProperty("prefix")]
+        public string Prefix { get; private set; }
+    }
+}
---
+++ DiscordBotSW20/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DiscordBotSW20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+        }
+    }
+}
