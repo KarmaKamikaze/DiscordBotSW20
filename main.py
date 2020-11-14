a4bedcbc338c298f3dcbe0bf7fc99579fdef22d1
KarmaKamikaze <nicolaijgn@gmail.com>
2020-11-14 17:35:56 +0100

    Switched project to Python 3.9

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from discord.ext import commands
+from settings import *
+
+bot = commands.Bot(command_prefix="!")
+
+
+
+# Add cogs from files
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py") and filename != "__init__.py":
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+
+bot.run(DISCORD_BOT_TOKEN)
---
+++ settings.py
@@ -0,0 +1,14 @@
+import os
+
+DEBUG = os.getenv("DEBUG", False)
+
+if DEBUG:
+    print("You are now in DEBUG mode!")
+    from pathlib import Path
+    from dotenv import load_dotenv
+    env_path = Path(".") / ".env.debug"
+    load_dotenv(dotenv_path=env_path)
+    from settings_files.development import *
+else:
+    print("You are now in PRODUCTION mode!")
+    from settings_files.production import *
