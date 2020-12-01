1509898643fc09a78fb4952fb36eef0d6bf469c4
KarmaKamikaze <nicolaijgn@gmail.com>
2020-12-01 20:57:02 +0100

    Rock Paper Scissors game module with parser and model

--- cogs/gambling.py
+++ cogs/gambling.py
@@ -1,5 +1,7 @@
 import random
 from discord.ext import commands
+from rps.model import RPS
+from rps.parser import rock_paper_scissors_parser
 
 
 class Gambling(commands.Cog):
@@ -26,6 +28,52 @@ class Gambling(commands.Cog):
     async def dice(self, ctx, dice_sides=20):
         number = random.randrange(1, dice_sides)
         await ctx.send(number)
+
+    @commands.command(
+        description="!rps scissors. Defaults to rock.",
+        brief="Play a game of Rock, Paper, Scissors against the bot.",
+        usage="rock | paper | scissors",
+    )
+    # Defaults to rock by setting the user_choice equal to another parser instance of RPS.ROCK
+    async def rps(
+        self,
+        ctx,
+        user_choice: rock_paper_scissors_parser = rock_paper_scissors_parser(RPS.ROCK),
+    ):
+        rps_model = RPS()
+        bot_choice = random.choice(rps_model.get_choices())
+        user_choice = user_choice.choice
+
+        # User is first parameter, bot is second
+        winner_check = {
+            (RPS.ROCK, RPS.PAPER): False,
+            (RPS.ROCK, RPS.SCISSORS): True,
+            (RPS.PAPER, RPS.ROCK): True,
+            (RPS.PAPER, RPS.SCISSORS): False,
+            (RPS.SCISSORS, RPS.ROCK): False,
+            (RPS.SCISSORS, RPS.PAPER): True,
+        }
+
+        won = None
+        if bot_choice == user_choice:
+            won = None
+        else:
+            won = winner_check[(user_choice, bot_choice)]
+
+        if won is True:
+            message = "You win: %s beats %s!" % (
+                user_choice.title(),
+                bot_choice.title(),
+            )
+        elif won is False:
+            message = "You lose: %s does not beat %s!" % (
+                user_choice.title(),
+                bot_choice.title(),
+            )
+        else:
+            message = "It's a draw!"
+
+        await ctx.send(message)
 
 
 def setup(bot):
---
+++ rps/model.py
@@ -0,0 +1,7 @@
+class RPS:
+    ROCK = "rock"
+    PAPER = "paper"
+    SCISSORS = "scissors"
+
+    def get_choices(self):
+        return (self.ROCK, self.PAPER, self.SCISSORS)
